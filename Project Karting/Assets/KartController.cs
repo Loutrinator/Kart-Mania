5e107eb787c13391ef20a0af762f7bad74d53b9c
Antoine Pavy <antoine.pavy92@gmail.com>
2020-08-22 19:51:59 +0200

    :muscle: Working gravity detection + new base material

--- Project Karting/Assets/KartController.cs
+++ Project Karting/Assets/KartController.cs
@@ -5,7 +5,23 @@ public class KartController : MonoBehaviour
 {
     #region PUBLIC VARIABLES
     
-    public KartStats stats;
+    public KartStats stats = new KartStats
+    {
+        topSpeed            = 10f,
+        acceleration        = 5f,
+        accelerationCurve   = .4f,
+        braking             = 10f,
+        reverseAcceleration = 5f,
+        reverseSpeed        = 5f,
+        steer               = 5f,
+        coastingDrag        = 4f,
+        grip                = .95f,
+        addedGravity        = 1f,
+        suspension          = .2f
+    };
+    
+    public Transform[] wheels = new Transform[0];
+    public float groundDetectionDist = 0.2f;
     
     #endregion
     
@@ -17,14 +33,15 @@ public class KartController : MonoBehaviour
     private Vector2 inputVector;
     //Physics
     private Vector3 gravity;
-    private float gravityLerpSpeed = 10f;
+    public float gravityLerpSpeed = 10f;
     [SerializeField,Range(0f,1f)]
     private float onGroundPercent = 1f;
     
     #endregion
     
-    //Temporary variables
+    #region TEMPORARY VARIABLES
     public Transform gravityObject;
+    #endregion
     
     #region SETUP
 
@@ -60,33 +77,41 @@ public class KartController : MonoBehaviour
     #region UPDATE
     private void Update()
     {
-        computeGravity();
         getInputs();
     }
     private void FixedUpdate()
     {
-        OnGroundMovement();
-        InAirMovement();
+        computeGravity();
+        // calcule le pourcentage de "en l'air" ou "au sol" du vehicule
+        var groundedCount = countGroundedWheels(out var minHeight);
+        onGroundPercent = (float)groundedCount / wheels.Length;
+        onGroundMovement();
+        gravityPhysics();
     }
 
-    #region PHYSICS
-    private void computeGravity()
+    private int countGroundedWheels(out float minHeight)
     {
-        gravity = Vector3.Lerp(gravity, gravityObject.forward, gravityLerpSpeed*Time.deltaTime);
+        //compte le nombre de roue au sol
+        int groundedCount = 0;
+        //on initialise minheight à la valeur la plus haute
+        minHeight = float.MaxValue;
+        //pour chaque roue
+        for (var i = 0; i < wheels.Length; ++i)
+        {
+            //la roue actuelle
+            Transform current = wheels[i];
+            //condition ternaire qui ajoute 1 à groundedCount si on a touché le sol
+            groundedCount += Physics.Raycast(current.position, -transform.up, out RaycastHit hit, groundDetectionDist) ? 1 : 0;
+            
+            if (hit.distance > 0)
+            {
+                //On stocke la plus petite distance avec le sol
+                minHeight = Mathf.Min(hit.distance, minHeight);
+            }
+        }
+        return groundedCount;
     }
 
-    private void OnGroundMovement()
-    {
-        rb.AddForce(onGroundPercent*transform.forward*inputVector.y);
-    }
-
-    private void InAirMovement()
-    {
-        float inAirPercent = 1f - onGroundPercent;
-        rb.AddForce(inAirPercent*gravity,ForceMode.Acceleration);
-    }
-    #endregion
-    
     private void getInputs()
     {
         inputVector = inputs.GenerateInput();
@@ -94,11 +119,52 @@ public class KartController : MonoBehaviour
 
     #endregion
 
+    #region PHYSICS
+    private void computeGravity()
+    {
+        Vector3 normalSum = Vector3.zero;
+        int didHit = 0;
+        for(int i = 0; i<wheels.Length; ++i)
+        {
+            if (Physics.Raycast(wheels[i].position+(transform.up*0.5f), -transform.up, out var hit, 3f))
+            {
+                normalSum += hit.normal;
+                didHit += 1;
+            }
+        }
+
+        normalSum /= didHit;
+        if (didHit > 0 )
+        {
+            Vector3 newGravity =  -normalSum;
+            gravity = Vector3.Lerp(gravity, newGravity, gravityLerpSpeed*Time.fixedDeltaTime);
+        }
+    }
+
+    private void onGroundMovement()
+    {
+        rb.AddForce(onGroundPercent*inputVector.y*stats.acceleration*transform.forward,ForceMode.Acceleration);
+    }
+
+    private void gravityPhysics()
+    {
+        float inAirPercent = 1f - onGroundPercent;
+        rb.AddForce(gravity,ForceMode.Acceleration);
+        rb.AddForce(stats.addedGravity*inAirPercent*gravity,ForceMode.Acceleration);//add gravity in mid air
+    }
+    #endregion
+    
     #region DEBUG
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawRay(transform.position,gravity);
+        
+        Gizmos.color = Color.green;
+        for(int i = 0; i<wheels.Length; ++i)
+        {
+            Gizmos.DrawRay(wheels[i].position+(transform.up*0.5f),-transform.up * 3f);
+        }
     }
     #endregion
     
