7ee1fb75bc87539fbe6cd4dafa0129486e181044
Antoine Pavy <antoine.pavy92@gmail.com>
2020-08-22 17:06:49 +0200

    :boom: Kart controller scratched to make proper one
    Working gravity smoothing

---
+++ Project Karting/Assets/KartController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody),typeof(KartInput))]
+public class KartController : MonoBehaviour
+{
+    #region PUBLIC VARIABLES
+    public KartStats stats;
+    #endregion
+    
+    #region PRIVATE VARIABLES
+    private Rigidbody rb;
+    private BaseInput inputs;
+    
+    //Physics
+    private Vector3 gravity;
+    public float gravityLerpSpeed;
+    #endregion
+    
+    //Temporary variables
+    public Transform gravityObject;
+    
+    #region SETUP
+    
+    private void Start()
+    {
+        setupRigidbody();
+        setupInputs();
+        gravity = Vector3.down;
+    }
+    
+    private void setupRigidbody()
+    {
+        if (rb) return;
+        rb = this.GetComponent<Rigidbody>();
+        if (rb) return;
+        rb = this.gameObject.AddComponent<Rigidbody>();
+    }
+    private void setupInputs()
+    {
+        if (inputs) return;
+        inputs = this.GetComponent<BaseInput>();
+        if (inputs) return;
+        inputs = this.gameObject.AddComponent<KartInput>();
+    }
+    #endregion
+    
+    #region UPDATE
+    private void Update()
+    {
+        computeGravity();
+    }
+
+    private void computeGravity()
+    {
+        gravity = Vector3.Lerp(gravity, gravityObject.forward, gravityLerpSpeed*Time.deltaTime);
+    }
+    #endregion
+
+    #region DEBUG
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position,gravity);
+    }
+    #endregion
+    
+}
