d06e477ae459059e04a8aa64040481e6ae8f6b48
Antoine Pavy <antoine.pavy92@gmail.com>
2020-08-24 22:24:25 +0200

    :bug: Fixed controls : removed Raw

--- Project Karting/Assets/KartInput.cs
+++ Project Karting/Assets/KartInput.cs
@@ -8,6 +8,6 @@ public class KartInput : BaseInput
     public override Vector2 GenerateInput()
     {
         //print("inputs : " + Input.GetAxisRaw("Horizontal") + " " + Input.GetAxisRaw("Vertical"));
-        return new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        return new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
     }
 }
