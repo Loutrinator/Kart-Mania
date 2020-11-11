46e39460206a23f1f69cbc882bc5c580645de315
Antoine Pavy <antoine.pavy92@gmail.com>
2020-11-11 11:00:47 +0100

    :sparkles: added race init + PlayerRaceInfo

---
+++ Project Karting/Assets/Scripts/PlayerRaceInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PlayerRaceInfo
+{
+    public KartBase kart;
+    public int playerId;//on sait jamais
+    public int lap;//the current lap
+    public int position;//the kart's position in te race (1rst, 2nd etc)
+    public int currentCheckpoint;//the previous checkpoint passed
+}
