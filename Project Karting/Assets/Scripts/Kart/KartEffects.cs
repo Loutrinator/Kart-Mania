e660bfdc73dc45c13ef002bc8cd0a1c9479039ba
Antoine Pavy <antoine.pavy92@gmail.com>
2020-11-26 21:37:15 +0100

    :ambulance::construction: HOTFIX fixed merge conflicts

--- Project Karting/Assets/Scripts/Kart/KartEffects.cs
+++ Project Karting/Assets/Scripts/Kart/KartEffects.cs
@@ -2,35 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Kart {
-    public class KartEffects : MonoBehaviour
-    {
-        public Camera camera;
-        public List<TrailRenderer> skidEmitters;
-        public List<ParticleSystem> driftSmokeEmitters;
-    
-    
-        public List<ParticleSystem> boostSparksEmitters;
-        public Light boostLight;
-        public Color[] boostColors;
-        [SerializeField] private float[] boostIntensity;
-        public float boostSwitchSpeed;
-        public float boostTimeToSwitch;
-        public AnimationCurve boostCameraEffect;
-        public float boostFOVOffset;
-        public float boostZOffset;
-        private bool boostActivated;
-        private float boostStrength;
-        private float boostLength;
-        private float boostStartTime;
-    
-    
-        private float currentIntensity;
-        private float baseFOV;
-        private float baseZ;
-    
-        //BoostEffect
-    
+public class KartEffects : MonoBehaviour
+{
+    public Camera camera;
+    public List<TrailRenderer> skidEmitters;
+    public List<ParticleSystem> driftSmokeEmitters;
+
+
+    public List<ParticleSystem> boostSparksEmitters;
+    public Light boostLight;
+    public Color[] boostColors;
+    [SerializeField] private float[] boostIntensity;
+    public float boostSwitchSpeed;
+    public float boostTimeToSwitch;
+    public AnimationCurve boostCameraEffect;
+    public float boostFOVOffset;
+    public float boostZOffset;
+    private bool boostActivated;
+    private float boostStrength;
+    private float boostLength;
+    private float boostStartTime;
+
+
+    private float currentIntensity;
+    private float baseFOV;
+    private float baseZ;
+
+    //BoostEffect
+
     [HideInInspector] public int driftLevel;
     public void Start()
     {
@@ -40,12 +39,13 @@ namespace Kart {
         baseFOV = camera.fieldOfView;
         baseZ = camera.transform.localPosition.z;
     }
-
+    /*
     public void LateUpdate()
     {
         //Debug.Log("currentIntensity = " + currentIntensity);
         //Debug.Log("driftLevel = " + driftLevel);
-        currentIntensity = Mathf.Lerp(currentIntensity, boostIntensity[driftLevel], Time.fixedDeltaTime * boostSwitchSpeed);
+        currentIntensity = Mathf.Lerp(currentIntensity, boostIntensity[driftLevel],
+            Time.fixedDeltaTime * boostSwitchSpeed);
         if (Mathf.Abs(currentIntensity - boostIntensity[driftLevel]) > 0.1f)
         {
             stopBoost();
@@ -54,111 +54,111 @@ namespace Kart {
             baseFOV = camera.fieldOfView;
             baseZ = camera.transform.localPosition.z;
         }
+    }*/
 
-        public void LateUpdate()
+    public void LateUpdate()
+    {
+        #if UNITY_EDITOR
+        Debug.Log("currentIntensity = " + currentIntensity);
+        Debug.Log("driftLevel = " + driftLevel);
+        #endif
+        currentIntensity = Mathf.Lerp(currentIntensity, boostIntensity[driftLevel], Time.fixedDeltaTime * boostSwitchSpeed);
+        if (Mathf.Abs(currentIntensity - boostIntensity[driftLevel]) > 0.1f)
         {
-            #if UNITY_EDITOR
-            Debug.Log("currentIntensity = " + currentIntensity);
-            Debug.Log("driftLevel = " + driftLevel);
-            #endif
-            currentIntensity = Mathf.Lerp(currentIntensity, boostIntensity[driftLevel], Time.fixedDeltaTime * boostSwitchSpeed);
-            if (Mathf.Abs(currentIntensity - boostIntensity[driftLevel]) > 0.1f)
-            {
-                boostLight.intensity = currentIntensity;
-            }
-            else
-            {
-                boostLight.intensity = boostIntensity[driftLevel];
-            }
+            boostLight.intensity = currentIntensity;
+        }
+        else
+        {
+            boostLight.intensity = boostIntensity[driftLevel];
+        }
 
-            if (boostActivated)
+        if (boostActivated)
+        {
+            float elapsed = Time.time - boostStartTime;
+            if (elapsed < boostLength)
             {
-                float elapsed = Time.time - boostStartTime;
-                if (elapsed < boostLength)
-                {
-                    float boostEffect =  boostCameraEffect.Evaluate(elapsed /boostLength)*boostStrength;
-                    camera.fieldOfView = baseFOV + boostEffect * boostFOVOffset;
-                    Vector3 previousPos = camera.transform.localPosition;
-                    camera.transform.localPosition =  new Vector3(previousPos.x,previousPos.y, baseZ - boostEffect * boostZOffset);
-            
-                }else
-                {
-                    stopBoost();
-                }
-            }
+                float boostEffect =  boostCameraEffect.Evaluate(elapsed /boostLength)*boostStrength;
+                camera.fieldOfView = baseFOV + boostEffect * boostFOVOffset;
+                Vector3 previousPos = camera.transform.localPosition;
+                camera.transform.localPosition =  new Vector3(previousPos.x,previousPos.y, baseZ - boostEffect * boostZOffset);
         
-        }
-
-        private IEnumerator BoostLoading()
-        {
-            WaitForSeconds wait = new WaitForSeconds(boostTimeToSwitch);
-            yield return wait;
-            driftLevel = 1;
-            boostLight.color = boostColors[driftLevel];
-            yield return wait;
-            driftLevel = 2;
-            boostLight.color = boostColors[driftLevel];
-            yield return wait;
-            driftLevel = 3;
-            boostLight.color = boostColors[driftLevel];
-        }
-        public void startDrift()
-        {
-            foreach(var skid in skidEmitters)
+            }else
             {
-                skid.emitting = true;
+                stopBoost();
             }
-            foreach(var smoke in driftSmokeEmitters)
-            {
-                smoke.Play();
-            }
-            driftLevel = 0;
-            boostLight.gameObject.SetActive(true);
-            StartCoroutine(BoostLoading());
-        }
-
-        public void stopDrift()
-        {
-            foreach (var skid in skidEmitters)
-            {
-                skid.emitting = false;
-            }
-
-            foreach(var smoke in driftSmokeEmitters)
-            {
-                smoke.Stop();
-            }
-            boostLight.color = boostColors[0];
-            currentIntensity = boostIntensity[0];
-            boostLight.gameObject.SetActive(true);
-            driftLevel = 0;
         }
     
-        public void startBoost(float length, float force)
-        {
-            foreach(var spark in boostSparksEmitters)
-            {
-                spark.Play();
-            }
+    }
 
-            boostLength = length;
-            boostStrength = force;
-            boostStartTime = Time.time;
-            boostActivated = true;
-        }
-        public void stopBoost()
+    private IEnumerator BoostLoading()
+    {
+        WaitForSeconds wait = new WaitForSeconds(boostTimeToSwitch);
+        yield return wait;
+        driftLevel = 1;
+        boostLight.color = boostColors[driftLevel];
+        yield return wait;
+        driftLevel = 2;
+        boostLight.color = boostColors[driftLevel];
+        yield return wait;
+        driftLevel = 3;
+        boostLight.color = boostColors[driftLevel];
+    }
+    public void startDrift()
+    {
+        foreach(var skid in skidEmitters)
         {
-            foreach(var spark in boostSparksEmitters)
-            {
-                spark.Stop();
-            }
-
-            boostActivated = false;
+            skid.emitting = true;
         }
+        foreach(var smoke in driftSmokeEmitters)
+        {
+            smoke.Play();
+        }
+        driftLevel = 0;
+        boostLight.gameObject.SetActive(true);
+        StartCoroutine(BoostLoading());
+    }
+
+    public void stopDrift()
+    {
+        foreach (var skid in skidEmitters)
+        {
+            skid.emitting = false;
+        }
+
+        foreach(var smoke in driftSmokeEmitters)
+        {
+            smoke.Stop();
+        }
+        boostLight.color = boostColors[0];
+        currentIntensity = boostIntensity[0];
+        boostLight.gameObject.SetActive(true);
+        driftLevel = 0;
+    }
+
+    public void startBoost(float length, float force)
+    {
+        foreach(var spark in boostSparksEmitters)
+        {
+            spark.Play();
+        }
+
+        boostLength = length;
+        boostStrength = force;
+        boostStartTime = Time.time;
+        boostActivated = true;
+    }
+    public void stopBoost()
+    {
+        foreach(var spark in boostSparksEmitters)
+        {
+            spark.Stop();
+        }
+
+        boostActivated = false;
+    }
+
+    public void startDriftLoading(int level)
+    {
     
-        public void startDriftLoading(int level)
-        {
-        
-        }
     }
 }
