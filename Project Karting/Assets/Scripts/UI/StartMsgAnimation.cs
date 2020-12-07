8174ee9a59a862edd8815a873f6c7a7d1db4b8d8
Tom <t.rakoto76@gmail.com>
2020-12-07 15:22:29 +0100

    go scale  animation is different

--- Project Karting/Assets/Scripts/UI/StartMsgAnimation.cs
+++ Project Karting/Assets/Scripts/UI/StartMsgAnimation.cs
@@ -10,6 +10,7 @@ public class StartMsgAnimation : MonoBehaviour
     [SerializeField] private AnimationCurve alphaCurve = null;
     [SerializeField] private AnimationCurve rotationCurve = null;
     [SerializeField] private AnimationCurve scaleCurve = null;
+    [SerializeField] private AnimationCurve goScaleCurve = null;
     [SerializeField] private float fadeInDuration = 1f;
     private float _elpasedTime;
     [HideInInspector] public float startTime;
@@ -45,17 +46,25 @@ public class StartMsgAnimation : MonoBehaviour
 
     public void StartScaleAnimation()
     {
+      
         if(GameManager.Instance.raceHasBegan()) return;
         _placeholder.sprite = icons[_iconIndex];
         _elpasedTime = Time.time - startTime;
         _step = _elpasedTime / fadeInDuration;
-        _scale = scaleCurve.Evaluate(_step) * Vector3.one;
-        
+        if (_iconIndex == 3)
+        {
+            _scale = goScaleCurve.Evaluate(_step) * Vector3.one;
+        }
+        else
+        {
+            _scale = scaleCurve.Evaluate(_step) * Vector3.one;
+            _placeholder.rectTransform.rotation = Quaternion.Euler(0,0,rotationCurve.Evaluate(_step));
+        }
+       
         _placeholder.transform.localScale = _scale;
         var tmp = _placeholder.color;
         tmp.a = alphaCurve.Evaluate(_step);
         _placeholder.color = tmp;
-        _placeholder.rectTransform.rotation = Quaternion.Euler(0,0,rotationCurve.Evaluate(_step));
         if (_step < 1) return;
         if (_iconIndex > 2)
         {
