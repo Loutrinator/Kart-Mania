5df3a4a83609a64639fdeb238b087186e8feb414
Tom <t.rakoto76@gmail.com>
2021-06-09 20:16:59 +0200

    fix checkpoints system

--- Project Karting/Assets/Scripts/Handlers/GameManager.cs
+++ Project Karting/Assets/Scripts/Handlers/GameManager.cs
@@ -132,6 +132,8 @@ namespace Handlers {
 
         public void CheckpointPassed(int checkpointId, int playerId) {
             PlayerRaceInfo player = playersInfo[playerId];
+            Debug.Log("id :" + checkpointId);
+
             //permet de vérifier si premièrement le checkpoint est valide et si il est après le checkpoint actuel
             if (checkpointId < checkpointAmount) {
                 if (checkpointId - player.currentCheckpoint == 1) {
