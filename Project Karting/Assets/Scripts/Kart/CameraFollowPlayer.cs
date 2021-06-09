e5d14e8fa27cb7044ee3c12d78a8f80a96b9a113
Antoine Pavy <antoine.pavy92@gmail.com>
2021-06-09 19:27:34 +0200

    Added second camera

--- Project Karting/Assets/Scripts/Kart/CameraFollowPlayer.cs
+++ Project Karting/Assets/Scripts/Kart/CameraFollowPlayer.cs
@@ -1,10 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Kart {
     public class CameraFollowPlayer : MonoBehaviour
     {
         public Transform target;
         public float rotationPercent = 0.1f;
+        public Camera frontCamera;
+        public Camera rearCamera;
+
  
         private void LateUpdate() {
             transform.position = target.position;
