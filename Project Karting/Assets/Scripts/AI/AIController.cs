9a7e8ca2b64597217f2903a4f76f2c831f8fa3db
Antoine Pavy <antoine.pavy92@gmail.com>
2021-09-15 15:32:15 +0200

    WIP of UtilityAI

---
+++ Project Karting/Assets/Scripts/AI/AIController.cs
@@ -0,0 +1,7 @@
+﻿namespace AI
+{
+    public interface AIController
+    {
+        
+    }
+}
---
+++ Project Karting/Assets/Scripts/Player/PlayerAI.cs
@@ -0,0 +1,80 @@
+using Handlers;
+using System.Linq;
+using Kart;
+using UnityEditor;
+using UnityEngine;
+
+using AI;
+using AI.UtilityAI;
+
+public class PlayerAI : MonoBehaviour
+{
+    [SerializeField] private KartBase _kart;
+    [SerializeField] private AIController _ai;
+
+    private void Awake()
+    {
+        var kart = GetComponent<KartBase>();
+        if (_ai == null)
+        {
+            _ai = new UtilityAIController();
+        }
+    }
+
+    private void AIUpdate()
+    {
+        
+    }
+
+    private void Input_OnActionTriggered(InputAction.CallbackContext ctx)
+    {
+        if (ctx.action.name == _controls.Kart.Movement.name)
+        {
+            OnMove(ctx);
+        }
+        if (ctx.action.name == _controls.Kart.Drift.name)
+        {
+            OnDrift(ctx);
+        }
+        if (ctx.action.name == _controls.Kart.Rearcamera.name)
+        {
+            OnRearCamera(ctx);
+        }
+        if (ctx.action.name == _controls.Kart.Pause.name)
+        {
+            OnPause(ctx);
+        }
+    }
+    
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (_kart != null)
+        {
+            Vector2 movement = context.ReadValue<Vector2>();
+            float x = movement[0] > 0.1f ? 1f : movement[0] < -0.1f ? -1f : 0;
+            float y = movement[1] > 0.1f ? 1f : movement[1] < -0.1f ? -1f : 0;
+            _kart.movement = new Vector2(x,y);
+        }
+    }
+
+    public void OnDrift(InputAction.CallbackContext context)
+    {
+        if (_kart != null)
+        {
+            _kart.drift = context.ReadValueAsButton();
+        }
+    }
+    public void OnRearCamera(InputAction.CallbackContext context)
+    {
+        if (_kart != null)
+        {
+            _kart.rear = context.ReadValue<float>() > 0 ;
+        }
+    }
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        GameManager.Instance.Pause();
+    }
+        
+}
