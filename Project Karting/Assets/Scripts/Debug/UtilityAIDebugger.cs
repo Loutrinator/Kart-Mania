9db4df9f92250fb63579adbc98093a9c4e035b19
Quentin <pamartqu@gmail.com>
2022-03-31 19:44:38 +0200

    merge fix

--- Project Karting/Assets/Scripts/Debug/UtilityAIDebugger.cs
+++ Project Karting/Assets/Scripts/Debug/UtilityAIDebugger.cs
@@ -7,8 +7,7 @@ using Kart;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class UtilityAIDebugger : MonoBehaviour
-{
+public class UtilityAIDebugger : MonoBehaviour {
     [SerializeField] private PlayerAI AI;
 
     [SerializeField] private bool showControllerDebugger = false;
@@ -33,40 +32,35 @@ public class UtilityAIDebugger : MonoBehaviour
         _initialized = true;
     }
 
-    private void SetupUtilityAI()
-    {
-        funcDebugPos = new Vector3(20,25,0);
+    private void SetupUtilityAI() {
+        funcDebugPos = new Vector3(20, 25, 0);
         RectTransform prefabTransform = ActionDebuggerPrefab.GetComponent<RectTransform>();
         offset = prefabTransform.rect.height;
         AddFunctionDebugger("Vitesse");
         AddFunctionDebugger("Distance du centre de la route");
         AddFunctionDebugger("Courbure de la route");
         AddFunctionDebugger("Alignement avec la route");
-            
+
         //speed, distanceToCenterOfRoad, curvatureOfTheRoad,  alignedToTheRoad, constant, sineNormalized
-        
     }
 
-    private void AddFunctionDebugger(string funcName)
-    {
+    private void AddFunctionDebugger(string funcName) {
         funcDebugPos -= Vector3.down * offset;
-        FunctionDebugger functionDebugger = Instantiate(FunctionDebuggerPrefab, funcDebugPos, Quaternion.identity, container);
+        FunctionDebugger functionDebugger =
+            Instantiate(FunctionDebuggerPrefab, funcDebugPos, Quaternion.identity, container);
         functionDebugger.name = funcName + " Debugger";
         functionDebugger.SetName(funcName);
         functionDebuggers.Add(functionDebugger);
-
     }
 
-    private void SetupForAI()
-    {
-        Vector3 position = new Vector3(20,Screen.height,0);
+    private void SetupForAI() {
+        Vector3 position = new Vector3(20, Screen.height, 0);
         RectTransform prefabTransform = ActionDebuggerPrefab.GetComponent<RectTransform>();
         float offset = prefabTransform.rect.height;
 
         List<string> names = AI.aiController.getActionNames();
         Debug.Log("NAME COUNT " + names.Count);
-        for (int i = 0; i < names.Count; ++i)
-        {
+        for (int i = 0; i < names.Count; ++i) {
             string actionName = names[i];
             position += Vector3.down * offset;
             ActionDebugger actionDebugger = Instantiate(ActionDebuggerPrefab, position, Quaternion.identity, container);
@@ -81,42 +75,36 @@ public class UtilityAIDebugger : MonoBehaviour
         float[] values = AI.aiController.getActionValues();
         int selectedId = AI.aiController.getSelectedActionId();
         int i;
-        for (i = 0; i < values.Length; ++i)
-        {
+        for (i = 0; i < values.Length; ++i) {
             float value = values[i];
-            if (i < actionDebuggers.Count)
-            {
+            if (i < actionDebuggers.Count) {
                 ActionDebugger debugger = actionDebuggers[i];
-                debugger.setValues(value,i == selectedId);
+                debugger.setValues(value, i == selectedId);
             }
         }
 
         KartBase kart = AI.kart;
-        UpdateFuncDebugger(0,UtilityAIKartBehaviorManager.Instance.SpeedFunction(kart));
-        UpdateFuncDebugger(1,UtilityAIKartBehaviorManager.Instance.DistanceToCenterOfRoadFunction(kart));
-        UpdateFuncDebugger(2,UtilityAIKartBehaviorManager.Instance.CurvatureOfRoadFunction(kart));
-        UpdateFuncDebugger(3,UtilityAIKartBehaviorManager.Instance.AlignedToRoad(kart));
+        UpdateFuncDebugger(0, UtilityAIKartBehaviorManager.Instance.SpeedFunction(kart));
+        UpdateFuncDebugger(1, UtilityAIKartBehaviorManager.Instance.DistanceToCenterOfRoadFunction(kart));
+        UpdateFuncDebugger(2, UtilityAIKartBehaviorManager.Instance.CurvatureOfRoadFunction(kart));
+        UpdateFuncDebugger(3, UtilityAIKartBehaviorManager.Instance.AlignedToRoad(kart));
 
-        if (showControllerDebugger)
-        {
+        if (showControllerDebugger) {
             controllerDebugger.gameObject.SetActive(true);
             controllerDebugger.SetInputs(AI.movement);
         }
-        else
-        {
+        else {
             controllerDebugger.gameObject.SetActive(false);
         }
-        
     }
 
-    private void UpdateFuncDebugger(int index, float value)
-    {
+    private void UpdateFuncDebugger(int index, float value) {
         FunctionDebugger debugger = functionDebuggers[index];
         debugger.SetValue(value);
     }
 
-    private void OnDrawGizmos()
-    {
-        UtilityAIKartBehaviorManager.Instance.OnDrawGizmos(AI.aiController.kart,debugArrowMesh);
+    private void OnDrawGizmos() {
+        if (!_initialized) return;
+        UtilityAIKartBehaviorManager.Instance.OnDrawGizmos(AI.kart, debugArrowMesh);
     }
 }
