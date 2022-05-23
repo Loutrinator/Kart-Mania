b43e048938c8c2bbd3d12ca5d44adffa47afc252
Antoine Pavy <antoine.pavy92@gmail.com>
2022-05-23 23:31:21 +0200

    Working Marble launcher

---
+++ Project Karting/Assets/Scripts/Items/Objects/Marble.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Items
+{
+    public class Marble : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer insideRenderer;
+        [SerializeField] private Rigidbody rb;
+        public void SetColor(Color color)
+        {
+            insideRenderer.material.SetColor("MainColor", color);
+        }
+
+        public void Freeze()
+        {
+            rb.isKinematic = true;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+        public void Shoot(Vector3 direction, float intensity)
+        {
+            transform.parent = null;
+            rb.isKinematic = false;
+            rb.constraints = RigidbodyConstraints.None;
+            rb.AddForce(direction*intensity,ForceMode.Impulse);
+            StartCoroutine(WaitForDissapear());
+        }
+
+        private IEnumerator WaitForDissapear()
+        {
+            yield return new WaitForSeconds(10f);
+            Destroy(gameObject);
+        }
+    }
+}
--- Project Karting/Assets/Scripts/Items/Objects/MarbleLauncher.cs
+++ Project Karting/Assets/Scripts/Items/Objects/MarbleLauncher.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 using DG.Tweening;
 using DG.Tweening.Core;
 using Items;
+using Random = UnityEngine.Random;
 
 public class MarbleLauncher : ItemObject
 {
+    [SerializeField] private Marble marblePrefab;
     [SerializeField] private LineRenderer leftRubber;
     [SerializeField] private LineRenderer rightRubber;
     [SerializeField] private List<Transform> leftRubberAttachment;
     [SerializeField] private List<Transform> rightRubberAttachment;
     [SerializeField] private List<Renderer> renderers;
     [SerializeField] private Transform marbleHolder;
-    [SerializeField] private Transform marble;
     [SerializeField] private float releaseSpeed = 0.25f;
+    [SerializeField] private float releaseTiming = 0.10f;
+    [SerializeField] private float throwIntensity = 0.01f;
     [SerializeField] private float stretchSpeed = 2f;
     [SerializeField] private float stretchMaxDistance = 5f;
     [SerializeField] private int maxMarbles = 3;
+    
 
     private Vector3 restPosition;
     private int remainingMarbles = 3;
+    private Marble currentMarble;
+    private bool marbleLocked;
 
     private void Start()
     {
@@ -31,7 +37,7 @@ public class MarbleLauncher : ItemObject
         ResetItem();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         leftRubber.SetPosition(0,leftRubberAttachment[0].position);
         leftRubber.SetPosition(1,leftRubberAttachment[1].position);
@@ -39,29 +45,53 @@ public class MarbleLauncher : ItemObject
         rightRubber.SetPosition(1,rightRubberAttachment[1].position);
     }
 
+    private void LateUpdate()
+    {
+        if(marbleLocked)
+        {
+            currentMarble.transform.position = marbleHolder.position;
+        }
+    }
+
     public void StretchRubber()
     {
-        Debug.Log("MarbleLauncher : StretchRubber");
         marbleHolder.DOKill();
         marbleHolder.DOLocalMove(restPosition - Vector3.forward * stretchMaxDistance,stretchSpeed).SetEase(Ease.OutCubic);
+        if (marbleLocked == false && remainingMarbles > 0)
+        {
+            Debug.Log("LOAD");
+            LoadMarble();
+        }
     }
     public void ShootMarble()
     {
-        Debug.Log("MarbleLauncher : ShootMarble");
         marbleHolder.DOKill();
         marbleHolder.DOLocalMove(restPosition, releaseSpeed).SetEase(Ease.OutElastic);
-        remainingMarbles--;
         
         StartCoroutine(waitToRelease());
     }
+
+    private void LoadMarble()
+    {
+        marbleLocked = true;
+        currentMarble = Instantiate(marblePrefab, marbleHolder.position, marbleHolder.rotation);
+        currentMarble.SetColor(Random.ColorHSV(0f, 1f, 0f, 0.9f, 1f, 1f));
+    }
     private IEnumerator waitToRelease()
     {
-        //0.128 is the time at wich in DOTween's EaseOutElastic reaches its peak value
-        yield return new WaitForSeconds(0.128f * releaseSpeed);
-        //Destroy(marble.gameObject);
+        Vector3 releasePos = marbleHolder.localPosition;
+        float distToRest = (releasePos - restPosition).magnitude;
+        
+        yield return new WaitForSeconds(releaseTiming * releaseSpeed);
+        remainingMarbles--;
+        marbleLocked = false;
+        Vector3 throwDirection = marbleHolder.forward;
+        float intensity = (distToRest / (releaseSpeed *releaseTiming))*throwIntensity;
+        currentMarble.Shoot(throwDirection, intensity);
         if (remainingMarbles <= 0)
         {
-            ResetItem();
+            yield return new WaitForSeconds(2f);
+            Destroy(gameObject);
         }
     }
 
