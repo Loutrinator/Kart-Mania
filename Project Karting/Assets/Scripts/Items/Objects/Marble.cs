c61b92011c1196e7fc0513aa86ac8f164f49c452
Antoine Pavy <antoine.pavy92@gmail.com>
2022-05-24 22:26:09 +0200

    v1 of marble shockwave

--- Project Karting/Assets/Scripts/Items/Objects/Marble.cs
+++ Project Karting/Assets/Scripts/Items/Objects/Marble.cs
@@ -1,12 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Items
 {
     public class Marble : MonoBehaviour
     {
+        [SerializeField] private GameObject marbleExplosionPrefab;
+        [SerializeField] private float minIntensityForShockwave = 3f;
         [SerializeField] private MeshRenderer insideRenderer;
         [SerializeField] private Rigidbody rb;
+        private GameObject marbleExplosion;
+        private bool shockwaveActivated = false;
+
+        public void LateUpdate()
+        {
+            if (shockwaveActivated)
+            {
+                marbleExplosion.transform.localRotation = Quaternion.LookRotation(Vector3.right,-rb.velocity);
+            }
+        }
+
         public void SetColor(Color color)
         {
             insideRenderer.material.SetColor("MainColor", color);
@@ -19,6 +33,12 @@ namespace Items
         }
         public void Shoot(Vector3 direction, float intensity)
         {
+            if (intensity > minIntensityForShockwave)
+            {
+                marbleExplosion = Instantiate(marbleExplosionPrefab, transform.position, transform.rotation,transform);
+                shockwaveActivated = true;
+            }
+            
             transform.parent = null;
             rb.isKinematic = false;
             rb.constraints = RigidbodyConstraints.None;
