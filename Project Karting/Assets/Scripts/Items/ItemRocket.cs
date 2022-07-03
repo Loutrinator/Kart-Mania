43005746aeb94f69599750a8cf5aa5985db1aade
Sikny <pamartqu@gmail.com>
2022-07-04 01:18:39 +0200

    started rocket

---
+++ Project Karting/Assets/Scripts/Items/ItemRocket.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Items {
+    [CreateAssetMenu(fileName = "Rocket", menuName = "ScriptableObjects/Items/Rocket")]
+    public class ItemRocket : Item {
+        public override void OnKeyHold(PlayerRaceInfo info) {
+            
+        }
+
+        public override void OnKeyDown(PlayerRaceInfo info) {
+            
+        }
+
+        public override void OnKeyUp(PlayerRaceInfo info) {
+            
+        }
+    }
+}
